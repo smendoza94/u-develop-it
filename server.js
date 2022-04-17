2c7d441a5f2041d91e966ebffb2e3f182877a699
StevenM <stevenbmendoza@gmail.com>
2022-04-17 14:51:10 -0700

    GET and DELETE endpoints for Parties db

--- server.js
+++ server.js
@@ -19,27 +19,27 @@ const db = mysql.createConnection(
     database: 'election'
   },
   console.log('mysql: Connected to the election database.')
-)
-
-// query method is the key component that allows 
-// SQL commands to be written in a Node.js application
-
-// GET all candidates
-app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT candidates.*, parties.name 
+  )
+  
+  // query method is the key component that allows 
+  // SQL commands to be written in a Node.js application
+  
+  // GET all candidates
+  app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name 
     AS party_name 
     FROM candidates 
     LEFT JOIN parties 
     ON candidates.party_id = parties.id`;
-  
-  db.query(sql, (err, rows) => {
-    // console.log(rows);
-    if (err) {
-      res.status(500).json({ error: err.message});
-      return;
-    }
-    res.json({
-      message: 'success',
+    
+    db.query(sql, (err, rows) => {
+      // console.log(rows);
+      if (err) {
+        res.status(500).json({ error: err.message});
+        return;
+      }
+      res.json({
+        message: 'success',
       data: rows
     });
   });
@@ -48,11 +48,11 @@ app.get('/api/candidates', (req, res) => {
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
   const sql = `SELECT candidates.*, parties.name 
-    AS party_name 
-    FROM candidates 
-    LEFT JOIN parties 
-    ON candidates.party_id = parties.id
-    WHERE candidates.id = ?`;
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id
+  WHERE candidates.id = ?`;
   const params = [req.params.id];
   
   db.query(sql, params, (err, row) => {
@@ -65,6 +65,58 @@ app.get('/api/candidate/:id', (req, res) => {
       message: 'success',
       data: row
     });
+  });
+});
+
+// GET endpoints for parties database API
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// DELETE endpoints for Parties API
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Party not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
   });
 });
 

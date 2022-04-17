9f83f65990156ab79c1048897337f63b69d7fc49
StevenM <stevenbmendoza@gmail.com>
2022-04-16 23:10:11 -0700

    express GET from sql query

--- server.js
+++ server.js
@@ -22,37 +22,61 @@ const db = mysql.createConnection(
 
 // query method is the key component that allows 
 // SQL commands to be written in a Node.js application
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  // console.log(rows);
+
+// GET all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+  
+  db.query(sql, (err, rows) => {
+    // console.log(rows);
+    if (err) {
+      res.status(500).json({ error: err.message});
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
 });
 
 // GET a single candidate
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(row);
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  
+  db.query(sql, params, (err, row) => {
+    // console.log(row);
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
 });
 
-// DELETE a candidate
-db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
-});
+// // DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 // CREATE a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-  VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//   VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
-});
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 // Express ROUTES
 // test route to ensure server is working
